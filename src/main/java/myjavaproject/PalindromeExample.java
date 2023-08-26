c0a64302bd54c92c17e72fc18dd77165adc84a04
konda659599 <126367666+konda659599@users.noreply.github.com>
2023-08-26 16:06:22 +0530

    Update PalindromeExample.java

--- src/main/java/myjavaproject/PalindromeExample.java
+++ src/main/java/myjavaproject/PalindromeExample.java
@@ -3,7 +3,7 @@ package myjavaproject;
 public class PalindromeExample {
 	public static void main(String args[]){  
 		  int r,sum=0,temp;    
-		  int n=4654;
+		  int n=47854;
 		  //It is the number variable to be checked for palindrome  
 		  
 		  temp=n;    
